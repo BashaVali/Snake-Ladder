1211eb76097550ca285f8406772ba1e309fe1aa2
BashaVali <bashavali222@gmail.com>
2022-10-31 10:06:20 +0530

    PlayerPositionZeroRestart

--- SnakeLadderProblem/Program.cs
+++ SnakeLadderProblem/Program.cs
@@ -11,6 +11,7 @@ namespace SnakeLadderProblem
             gamePlay.GamePlay();
             gamePlay.DiceRoll();
             gamePlay.CheckingPlayerPosition();
+            gamePlay.PlayerPositionZeroRestart();
 
         }
     }
